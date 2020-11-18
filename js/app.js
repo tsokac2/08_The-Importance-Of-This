a6d8c2a95fef16ddcba71c1a12940022422c6fe3
tsokac2 <tsokac2@gmail.com>
2020-11-18 10:32:35 +0000

    Adding .on(click) to app.js

--- js/app.js
+++ js/app.js
@@ -20,6 +20,17 @@
 //     });
 // });
 
+
+// $(".module-nav").on("click", function(){
+//   let allModulesCardsSelector = ".card";
+//   let thisModuleCardsSelector = "." + this.id + "-card";
+  
+//   $(allModulesCardsSelector).removeClass("card-highlight");
+//   $(thisModuleCardsSelector).addClass("card-highlight");
+  
+// });
+
+
 $(document).ready(function(){
     $(".box").on("click", function(){
         /** 
