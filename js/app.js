52dbf8decf355841e710e0b1e8683782848379ce
tsokac2 <tsokac2@gmail.com>
2020-11-19 18:07:46 +0000

    Add XHR server request functionality to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.send();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText;
+    }
+};
+
+
 // $(document).ready(function(){
 
 //     $("p").click(function(){
@@ -31,27 +43,27 @@
 // });
 
 
-$(document).ready(function(){
-    $(".box").on("click", function(){
-        /** 
-         * When we click on an elemnt that has a "box" class, this event will be fired
-        */
-       var classNames = $(this).attr("class").split(" ");
-       var boxClass = classNames[0];
-       var className = classNames[1];
-       if($(this).css("background-color") == "rgb(255, 0, 0)"){
-            //If this object is already red, turn it black
-            $("." + className).css("background-color", "#000");
-       } else {
-           // Else turn all elements with a box class black
-           // and then change the color of all elements
-           // with the same class name as the clicked element
-           // to red.
-           $("." + boxClass).css("background-color", "#000");
-           $("." + className).css("background-color", "red"); 
-       }
-    });
-});
+// $(document).ready(function(){
+//     $(".box").on("click", function(){
+//         /** 
+//          * When we click on an elemnt that has a "box" class, this event will be fired
+//         */
+//        var classNames = $(this).attr("class").split(" ");
+//        var boxClass = classNames[0];
+//        var className = classNames[1];
+//        if($(this).css("background-color") == "rgb(255, 0, 0)"){
+//             //If this object is already red, turn it black
+//             $("." + className).css("background-color", "#000");
+//        } else {
+//            // Else turn all elements with a box class black
+//            // and then change the color of all elements
+//            // with the same class name as the clicked element
+//            // to red.
+//            $("." + boxClass).css("background-color", "#000");
+//            $("." + className).css("background-color", "red"); 
+//        }
+//     });
+// });
 
 
 
